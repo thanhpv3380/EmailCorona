d7d033d3adfcf6678eaeab3e1c21a1072793a1c7
= <=>
2020-03-14 01:30:17 +0700

    fix mailer

--- sendEmail/sendEmail.js
+++ sendEmail/sendEmail.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 const sendEmail = (data) => {
-    let transporter = nodeMailer.createTransport({
+    let transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
